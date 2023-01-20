15b5325f02daaf702ae40f92c375923a5a4782d9
Ian Gwillim <igwillim@gmail.com>
2023-01-19 22:52:26 -0500

    Initial commit for L01

---
+++ L01/Echo/src/echo/EchoClient.java
@@ -0,0 +1,18 @@
+package echo;
+
+/**
+ *
+ * @author Team 2
+ */
+public class EchoClient
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+    }
+    
+}
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # GroupWork
 Repo for all semester group work.
+
+### Group Members
+- Aurora Drabik
+- Jang Cho
+- Tyler Cullen
+- Kaelynn Davies
+- Ian Gwillim
+- Andrew Kochhar
+- Ryan Waitlevertch
+
+### Versions
+- JDK 17
