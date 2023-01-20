0f2c58276aaf160e9c1232b5a7dd5ed65ea1e1f3
Aurora Blasko <amb1135@psu.edu>
2023-01-20 11:54:03 -0500

    Echo Client and Server code update from book, with catches and port change to 8080

--- L01/Echo/src/echo/EchoClient.java
+++ L01/Echo/src/echo/EchoClient.java
@@ -1,5 +1,16 @@
 package echo;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Team 2
@@ -12,7 +23,35 @@ public class EchoClient
      */
     public static void main(String[] args)
     {
-        // TODO code application logic here
+        System.out.println("Simple Echo Client");
+        
+        try {
+        System.out.println("Waiting for connection.....");
+        InetAddress localAddress = InetAddress.getLocalHost();
+
+        try (Socket clientSocket = new Socket(localAddress, 8080);
+                    PrintWriter out = new PrintWriter(
+                        clientSocket.getOutputStream(), true);
+                    BufferedReader br = new BufferedReader(
+                        new InputStreamReader(
+                        clientSocket.getInputStream()))) {
+            System.out.println("Connected to server");
+            Scanner scanner = new Scanner(System.in);
+            while (true) {
+                System.out.print("Enter text: ");
+                String inputLine = scanner.nextLine();
+                if ("quit".equalsIgnoreCase(inputLine)) {
+                    break;
+                }
+            out.println(inputLine);
+            String response = br.readLine();
+            System.out.println("Server response: " + response);
+        }
+        }   catch (IOException ex) {
+                Logger.getLogger(EchoClient.class.getName()).log(Level.SEVERE, null, ex);
+            }
+    }   catch (UnknownHostException ex) {
+            Logger.getLogger(EchoClient.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-    
 }
--- L01/Echo/src/echo/EchoServer.java
+++ L01/Echo/src/echo/EchoServer.java
@@ -1,5 +1,14 @@
 package echo;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Team 2
@@ -12,7 +21,25 @@ public class EchoServer
      */
     public static void main(String[] args)
     {
-        // TODO code application logic here
-    }
-    
+        System.out.println("Simple Echo Server");
+        
+        try (ServerSocket serverSocket = new ServerSocket(8080)){
+        System.out.println("Waiting for connection.....");
+        Socket clientSocket = serverSocket.accept();
+        System.out.println("Connected to client");
+        
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true)) {
+                String inputLine;
+                while ((inputLine = br.readLine()) != null) {
+                System.out.println("Server: " + inputLine);
+                out.println(inputLine);
+            }
+        }   catch (IOException ex) {
+                Logger.getLogger(EchoServer.class.getName()).log(Level.SEVERE, null, ex);
+            }
+    }   catch (IOException ex) {
+            Logger.getLogger(EchoServer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }    
 }
