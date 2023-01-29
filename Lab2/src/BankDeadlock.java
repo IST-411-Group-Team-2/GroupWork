06a6a7b555b15b3e5370daeebfe8166a7759b1a0
rjoewait <122944188+rjoewait@users.noreply.github.com>
2023-01-29 14:26:03 -0500

    Update BankDeadlock.java

--- Lab2/src/BankDeadlock.java
+++ Lab2/src/BankDeadlock.java
@@ -16,9 +16,13 @@ public class BankDeadlock {
     
     public static void main(String[] args)  {
     
+        //Initial bank object is generated using initial balance static and a set size of accounts (100)
         Bank bank = new Bank (NACCOUNTS, INITIAL_BALANCE);
+        
+        //Loop runs through all accounts to create and run threads
         for (int i = 0; i < NACCOUNTS; i++) {
             int fromAccount = i;
+            //Runnable thread created, calls the transfer method from Bank.java and displays tranfser info
             Runnable r = () -> {
                 try {
                     while (true) {
@@ -26,9 +30,11 @@ public class BankDeadlock {
                         double amount = MAX_AMOUNT * Math.random();
                         bank.transfer(toAccount, fromAccount, amount);
                     }
+                //Exception thrown if program is stopped
                 } catch (InterruptedException ex) {
                 }
             };
+            //Thread is made and ran
             Thread t = new Thread(r);
             t.start();
         }
