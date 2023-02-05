8a250fac071757475ca64c4bab039c828c60874c
Aurora Blasko <amb1135@psu.edu>
2023-02-05 10:03:24 -0500

    added comments for metadata and NoaaData

--- Lab3/NoaaProject/src/noaaproject/Metadata.java
+++ Lab3/NoaaProject/src/noaaproject/Metadata.java
@@ -5,15 +5,20 @@ Purpose Details: Connections NOAA Web Services
 Course: IST 411
 Author: Group 2 (Aurora Blasko Drabik, Jang Won Cho, Tyler Cullen, Kaelynn Davies, Ian Gwillim, Andrew Kochhar, Ryan Waitlevertch)
 Date Developed: 2/2/2023
-Last Date Changed:2/2/2023
+Last Date Changed:2/5/2023
 Revision: 1
 */
 public class Metadata {
     private ResultSet resultset;
     
+    /**
+     * @return the resultSet generated from the metadata
+     */
     public ResultSet getResultSet(){
         return resultset;
     }
+    
+    
     public void setResultSet(ResultSet rs){
         this.resultset = rs;
     }
--- Lab3/NoaaProject/src/noaaproject/NoaaData.java
+++ Lab3/NoaaProject/src/noaaproject/NoaaData.java
@@ -5,22 +5,37 @@ Purpose Details: Connections NOAA Web Services
 Course: IST 411
 Author: Group 2 (Aurora Blasko Drabik, Jang Won Cho, Tyler Cullen, Kaelynn Davies, Ian Gwillim, Andrew Kochhar, Ryan Waitlevertch)
 Date Developed: 2/2/2023
-Last Date Changed:2/2/2023
+Last Date Changed:2/5/2023
 Revision: 1
 */
 public class NoaaData {
     private Metadata metadata;
     private Results results[];
     
+    /**
+     * @return the meta data gathered from the NOAA website
+     */
     public Metadata getMetadata(){
         return metadata;
     }
+    
+    /**
+     * populates the metadata
+     */
     public void setMetadata(Metadata md){
         this.metadata = md;
     }
+    
+    /**
+     * @return the array of results gathered from the NOAA website
+     */
     public Results[] getResults(){
         return results;
     }
+    
+    /**
+     * populates the results data
+     */
     public void setResults(Results[] r){
         this.results = r;
     }
