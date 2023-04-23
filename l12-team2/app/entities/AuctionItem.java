825932dae18b9b7bb65f09267870763c38f2e2ac
rjoewait <122944188+rjoewait@users.noreply.github.com>
2023-04-22 23:33:16 -0400

    Update AuctionItem.java

--- l12-team2/app/entities/AuctionItem.java
+++ l12-team2/app/entities/AuctionItem.java
@@ -1,5 +1,6 @@
 package entities;
 
+//Class that generates an auction item entry
 public class AuctionItem {
     private int auctionID;
     private String auctionName;
@@ -8,6 +9,7 @@ public class AuctionItem {
     private double startingBid;
 
 
+    //Constructor method
     public AuctionItem(int auctionID, String auctionName, String sellerName, String itemDescription, double startingBid) {
         this.auctionID = auctionID;
         this.auctionName = auctionName;
@@ -16,48 +18,48 @@ public class AuctionItem {
         this.startingBid = startingBid;
     }
 
-
+    //Get method for an item's Auction ID
     public int getAuctionID() {
         return this.auctionID;
     }
-
+    //Set method for an item's Auction ID
     public void setAuctionID(int auctionID) {
         this.auctionID = auctionID;
     }
-
+    //Get method for an item's name
     public String getAuctionName() {
         return this.auctionName;
     }
-
+    //Set method for an item's name
     public void setAuctionName(String auctionName) {
         this.auctionName = auctionName;
     }
-
+    //get method for an item's seller name
     public String getSellerName() {
         return this.sellerName;
     }
-
+    //Set method for an item's seller name
     public void setSellerName(String sellerName) {
         this.sellerName = sellerName;
     }
-
+    //Get method for item description
     public String getItemDescription() {
         return this.itemDescription;
     }
-
+    //Get method for item description
     public void setItemDescription(String itemDescription) {
         this.itemDescription = itemDescription;
     }
-
+    //Get starting bid info
     public double getStartingBid() {
         return this.startingBid;
     }
-
+    //Set a starting bid
     public void setStartingBid(double startingBid) {
         this.startingBid = startingBid;
     }
 
-
+    //Output string method for all Auction Item info
     @Override
     public String toString() {
         return "{" +
